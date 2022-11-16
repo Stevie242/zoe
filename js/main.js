391ac17c629d5332ca0c892b58873f717401eca1
Stevie TATHY-LUBASSHY <stevietathy@gmail.com>
2022-11-16 11:49:38 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -13,36 +13,30 @@ $(document).ready(function(){
         var bg = $(this).data('setbg');
         $(this).css('background', 'linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(' + bg + ') no-repeat center center /cover');
     });
-
     $('.banniere').each(function () {
         var bg = $(this).data('setbg');
         $(this).css('background', 'linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(' + bg + ') no-repeat center center /cover');
     });
 
-    /*-------------------
-		Quantity change
-	--------------------- */
-    /*var proQty = $('.pro-qty');
-    proQty.prepend('<span class="dec qtybtn">-</span>');
-    proQty.append('<span class="inc qtybtn">+</span>');
-    proQty.on('click', '.qtybtn', function () {
-        var $button = $(this);
-        var oldValue = $button.parent().find('input').val();
-        if ($button.hasClass('inc')) {
-            var newVal = parseFloat(oldValue) + 1;
-        } else {
-            // Don't allow decrementing below zero
-            if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
-            } else {
-                newVal = 0;
-            }
-        }
-        $button.parent().find('input').val(newVal);
-    });*/
-
+    /*------------------
+        DataTable
+    --------------------*/
     $('#user-historique').DataTable({
         "bFilter": false,
     });
+
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $('#content').toggleClass('active');
+    });
+
+    $('.more-button,.body-overlay').on('click', function () {
+        $('#sidebar,.body-overlay').toggleClass('show-nav');
+    });
+
+    $('#menu-btn').click(function(){
+        $('#menu').toggleClass("active")
+    })
 
 })
