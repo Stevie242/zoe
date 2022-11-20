a97d3409b9435404e6ad46e872dbbfcd850ae3f1
Stevie TATHY-LUBASSHY <stevietathy@gmail.com>
2022-11-20 13:08:39 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-/*var loader = document.querySelector(".loader")
-window.addEventListener('load',vanish);
-function vanish(){
-    loader.classList.add ('disppear')
-}
-*/
 
 $(document).ready(function(){
     /*------------------
@@ -18,12 +12,27 @@ $(document).ready(function(){
         $(this).css('background', 'linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(' + bg + ') no-repeat center center /cover');
     });
 
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background', 'linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(' + bg + ') no-repeat center center /cover');
+    });
+    
+
+    $('#menu-toggle').click(function(){
+        $('.nav-mobile').toggleClass('active');
+    })
+
+    $('#close-btn').click(function(){
+        $('.nav-mobile').toggleClass('active');
+    })
     /*------------------
         DataTable
     --------------------*/
     $('#user-historique').DataTable({
         "bFilter": false,
     });
+
+    $('#user-dashboard').DataTable();
 
 
     $('#sidebarCollapse').on('click', function () {
