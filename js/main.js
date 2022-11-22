f2b926b582fd4065268ecc825d3218c3d636aab1
Stevie TATHY-LUBASSHY <stevietathy@gmail.com>
2022-11-22 16:29:51 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -48,6 +48,28 @@ $(document).ready(function(){
         $('#menu').toggleClass("active")
     })
 
+
+    /*Show picture upload */
+    let input = document.querySelector('#add_pic');
+    let img = document.querySelector('#image_profile');
+
+    input.addEventListener('change',()=>{
+        let file = input.files[0];
+
+        let btn = document.querySelector('#btn-add-pic');
+
+        if(file){
+            console.log(file);
+            let reader = new FileReader();
+            reader.addEventListener('load',() => img.src = reader.result);
+            reader.readAsDataURL(file);
+            btn.classList.add('file');
+        }else {
+            img.src = "img/user.png";
+            btn.classList.remove('file');
+        }
+    })
+
 })
 
 const boxes = document.querySelectorAll(['.col-content','.box-tarif','.about-text'])
@@ -64,3 +86,5 @@ function checkBoxes (){
         }
     })
 }
+
+
